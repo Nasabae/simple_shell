52da9bc90be1149091cb6eca936c3ee3a865dbf4
Nasabae <ssakoto11@gmail.com>
2023-06-23 15:21:12 -0700

    team project on simple shell

---
+++ ssa_getinfo.c
@@ -0,0 +1,71 @@
+#include "shell.h"
+
+/**
+ * ssa_clear_info - Initializes info_t structure
+ * @info: Structure address
+ */
+void ssa_clear_info(info_t *info)
+{
+info->arg = NULL;
+info->argv = NULL;
+info->path = NULL;
+info->argc = 0;
+}
+
+/**
+ * ssa_set_info - Initializes info_t structure
+ * @info: structure address
+ * @av: argument vector
+ */
+void ssa_set_info(info_t *info, char **av)
+{
+int z = 0;
+info->fname = av[0];
+if (info->arg)
+{
+info->argv = strtow(info->arg, "\t");
+if (!info->argv)
+{
+info->argv = malloc(sizeof(char *) * 2);
+if (info->argv)
+{
+info->argv[0] = _strdup(info->arg);
+info->argv[1] = NULL;
+}
+}
+for (z = 0; info->argv && info->argv[z]; z++)
+;
+info->argc = z;
+replace_alias(info);
+replace_vars(info);
+}
+}
+
+/**
+ * ssa_free_info - frees info_t structure fields
+ * @info: structure address
+ * @all: true if freeing all fields
+ */
+void ssa_free_info(info_t *info, int all)
+{
+ffree(info->argv);
+info->argv = NULL;
+info->path = NULL;
+if (all)
+{
+if (!info->cmd_buf)
+free(info->arg);
+if (info->env)
+free_list(&(info->env));
+if (info->history)
+free_list(&(info->history));
+if (info->alias)
+free_list(&(info->alias));
+ffree(info->environ);
+info->environ = NULL;
+bfree((void **)info->cmd_buf);
+if (info->readfd > 2)
+close(info->readfd);
+_putchar(BUF_FLUSH);
+}
+}
